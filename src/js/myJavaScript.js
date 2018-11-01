02a3da55ed7d25a45fdf0d619bfc4880aaa4c146
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-11-01 19:18:37 +0100

    cetvrtak 01.11.   javascript radjen

--- src/js/myJavaScript.js
+++ src/js/myJavaScript.js
@@ -3,6 +3,19 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $(this.hash).offset().top - 50}, 1000);
         return false;
     });
+
+
+   $('.wraper').mouseenter(function(){
+       $('#rollRed').addClass("rollRed");
+       $('#rollGray').addClass("rollGrayAnim");
+       $('#rollRed').removeClass("rollGray");
+   })
+
+   $('.wraper').mouseleave(function(){
+    $('#rollRed').removeClass("rollRed");
+
+})
+    
     });
 
 
