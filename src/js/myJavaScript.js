39bb16fba31d16cdeebacaf3700447c65dc04ca8
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-11-02 01:36:57 +0100

    od kuce javascript...scrollTop

--- src/js/myJavaScript.js
+++ src/js/myJavaScript.js
@@ -5,17 +5,34 @@ $(document).ready(function(){
     });
 
 
-   $('.wraper').mouseenter(function(){
-       $('#rollRed').addClass("rollRed");
-       $('#rollGray').addClass("rollGrayAnim");
-       $('#rollRed').removeClass("rollGray");
-   })
 
-   $('.wraper').mouseleave(function(){
-    $('#rollRed').removeClass("rollRed");
+    $(window).scroll(function(){
+        var scroll=$(window).scrollTop();
+           console.log(scroll);
+            if(scroll >= 999 ){
+                $('#rollRed').addClass("rollRed");
+                $('#rollGray').addClass("rollGrayAnim");
+                $('#rollRed').removeClass("rollGray");
+            }
+             if(scroll >= 3500){
+               $('#sec6-col1').addClass('apear');
 
+               setTimeout(function() {
+                $('#sec6-col2').addClass('apear');
+            }, 300);
+            setTimeout(function() {
+                $('#sec6-col3').addClass('apear');
+            }, 600);
+              
+            }
+            
+        });
+
+        $("#pisiteNam").click(function(){
+            $("#myForm2").slideDown("slow");
+        });
 })
     
-    });
+   
 
 
